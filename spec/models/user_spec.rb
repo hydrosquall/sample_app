501d9267a42db282304af63105228dd00e8adf22
Cameron Yick <cameron.yick@yale.edu>
2015-06-16 16:33:31 -0400

    finish user edit, update, index, and destroy sections

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,14 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean         default(FALSE)
 #
 
 require 'spec_helper'
@@ -23,8 +26,6 @@ describe User do
   it {should respond_to(:password) }
   it {should respond_to(:password_confirmation) }
 
- 
-
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:admin) }
@@ -36,7 +37,7 @@ describe User do
   describe "with admin attribute set to 'true'" do
     before { @user.toggle!(:admin) }
 
-    it {should be_admin}
+    it { should be_admin }
   end
 
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -81,8 +81,6 @@ describe "AuthenticationPages" do
 						end
 					end
 				end
-
-
 			end
 			
 			describe "as wrong user" do 
@@ -99,7 +97,18 @@ describe "AuthenticationPages" do
 					before { put user_path(wrong_user) }
 					specify { response.should redirect_to(root_path) }
 				end
+			end
 
+			describe "as non-admin user" do
+				let(:user) {FactoryGirl.create(:user) }
+				let(:non_admin) {FactoryGirl.create(:user) }
+
+				before {valid_sign_in non_admin }
+
+				describe "submitting a DELETE request to the Users#destroy action" do
+					before { delete user_path(user) }
+					specify {response.should redirect_to(root_path)}
+				end
 			end
 		end
 	end
